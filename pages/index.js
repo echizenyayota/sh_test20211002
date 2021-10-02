cd11907b0acc9847b9f984ec7d19e91f847024ab
echizenyayota <virtualmisso30@gmail.com>
2021-10-02 22:29:12 +0900

    Create the form

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,43 @@
-import { Heading, Page } from "@shopify/polaris";
+import { useState } from "react";
+import { Button, Card, Heading, Page, Stack, TextField } from "@shopify/polaris";
+import { ResourcePicker } from "@shopify/app-bridge-react";
 
-const Index = () => (
+
+const Index = () => {
+  const [appendToTitle, setAppendToTitle] = useState('');
+  const [appendToDescription, setAppendToDescription] = useState('');
+  const [pickerOpen, setPickerOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [showToast, setshowToast] = useState(false);
+
+  return(
     <Page>
       <Heading>Producter Updater App</Heading>
+      <Card>
+        <Card.Section>
+          <Stack vertical>
+            <TextField
+              label="Apped to title"
+              value={appendToTitle}
+              onChange={setAppendToTitle}
+            ></TextField>
+            <TextField
+              label="Apped to description"
+              value={appendToDescription}
+              onChange={setAppendToDescription}
+              multiline={3}
+            ></TextField>
+            <ResourcePicker
+            
+            />
+            <Button primary onClick={() => console.log('Clicked')}>Select Products</Button>
+          </Stack>
+        </Card.Section>
+      </Card>
     </Page>
-);
+  );
+
+}
+
 
 export default Index;
